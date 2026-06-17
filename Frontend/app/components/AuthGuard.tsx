3b118d9bef73c6a021a2b49b3214981cb7b8a197
yohan9878 <yohanasitha98@gmail.com>
2026-06-17 14:31:47 +0530

    Modified styles

--- Frontend/app/components/AuthGuard.tsx
+++ Frontend/app/components/AuthGuard.tsx
@@ -2,6 +2,7 @@
 
 import { ReactNode, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 type AuthGuardProps = {
   children: ReactNode;
@@ -20,16 +21,20 @@ export default function AuthGuard({ children }: AuthGuardProps) {
       } else {
         setLoading(false);
       }
-    }, 2000);
+    }, 5000);
 
     return () => clearTimeout(timer);
   }, [router]);
 
   if (loading) {
     return (
-      <div className="flex h-screen items-center justify-center bg-white">
-        <p className="text-center font-semibold text-red-900 ">
-          Checking authentication...
+      <div className="flex h-screen flex-col items-center justify-center bg-white text-red-800">
+        <div className="mb-6 h-12 w-12 animate-spin rounded-full border-4 border-slate-200 border-t-red-800"></div>
+
+        <h1 className="text-xl font-bold">WEEHENA FARM SHOP</h1>
+
+        <p className="mt-2 font-semibold text-red-800">
+          Authenticating user...
         </p>
       </div>
     );
