5824a7c39ca2ab16e429288034371822f7c32142
yohan9878 <yohanasitha98@gmail.com>
2026-06-20 11:03:49 +0530

    Implemented get stock history for period API

--- Backend/src/main/java/com/pos/pos_system_backend/repository/StockHistoryRepository.java
+++ Backend/src/main/java/com/pos/pos_system_backend/repository/StockHistoryRepository.java
@@ -2,8 +2,24 @@ package com.pos.pos_system_backend.repository;
 
 import com.pos.pos_system_backend.entity.StockHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface StockHistoryRepository extends JpaRepository<StockHistory, Long> {
 
+    @Query("""
+       SELECT sh
+       FROM StockHistory sh
+       WHERE sh.outletId = :outletId
+       AND sh.changedAt BETWEEN :startDate AND :endDate
+       ORDER BY sh.changedAt DESC
+       """)
+    List<StockHistory> getStockHistoryForPeriod(
+            @Param("outletId") String outletId,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate);
 }
 
